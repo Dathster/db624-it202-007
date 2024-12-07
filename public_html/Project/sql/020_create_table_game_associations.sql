f45b181ea7358a889825db1e9aec521f958c601c
Datha Bindumalam <db624@njit.edu>
2024-12-07 08:13:20 -0500

    Implemented button for user to save game to profile

--- public_html/Project/sql/020_create_table_game_associations.sql
+++ public_html/Project/sql/020_create_table_game_associations.sql
@@ -6,7 +6,7 @@ CREATE TABLE IF NOT EXISTS  `Game_associations`
     `created`    timestamp default current_timestamp,
     `modified`   timestamp default current_timestamp on update current_timestamp,
     PRIMARY KEY (`id`),
-    FOREIGN KEY (`user_id`) REFERENCES Users(`id`),
-    FOREIGN KEY (`game_id`) REFERENCES Games_details(`game_id`),
+    FOREIGN KEY (`user_id`) REFERENCES Users(`id`) on delete cascade,
+    FOREIGN KEY (`game_id`) REFERENCES Games_details(`game_id`) on delete cascade,
     UNIQUE KEY (`user_id`, `game_id`)
 )
