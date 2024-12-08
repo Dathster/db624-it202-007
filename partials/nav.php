4d4814aa4500b9f8ae9c9cfd72d5efe0585708d1
Datha Bindumalam <db624@njit.edu>
2024-12-07 22:50:59 -0500

    Completed page

--- partials/nav.php
+++ partials/nav.php
@@ -59,6 +59,7 @@ session_start();
             <li><a class="dropdown-item" href="<?php echo get_url('admin/list_roles.php'); ?>">List Roles</a></li>
             <li><a class="dropdown-item" href="<?php echo get_url('admin/assign_roles.php'); ?>">Assign Roles</a></li>
             <li><a class="dropdown-item" href="<?php echo get_url('admin/game_fetch.php'); ?>">Fetch/Insert Games</a></li>
+            <li><a class="dropdown-item" href="<?php echo get_url('admin/unsaved_games.php'); ?>">Unsaved Games</a></li>
           </ul>
         <?php endif; ?>
         <?php if (is_logged_in()) : ?>
--- public_html/Project/admin/unsaved_games.php
+++ public_html/Project/admin/unsaved_games.php
@@ -1,15 +1,15 @@
 <?php
-require(__DIR__ . "/../../partials/nav.php");
+require(__DIR__ . "/../../../partials/nav.php");
 ?>
 <div class='container-fluid'>
-    <h1>Home</h1>  
+    <h1>Unsaved Games</h1>  
 </div>
 
 <?php
 
-if (is_logged_in(true)) {
-    //comment this out if you don't want to see the session variables
-    error_log("Session data: " . var_export($_SESSION, true));
+if (!has_role("Admin")) {
+    flash("You don't have permission to view this page", "warning");
+    die(header("Location: $BASE_PATH" . "/home.php"));
 }
 ?>
 
@@ -20,26 +20,6 @@ if (is_logged_in(true)) {
 
     if(isset($_GET["reset"])){
         $_GET = [];
-    }
-    if(isset($_GET["remove_all"])){
-        $query_remove_all_game_associations = "delete from `Game_associations` where `user_id` = $user_id";
-        $db = getDB();
-        $stmt = $db->prepare($query_remove_all_game_associations);
-        try{
-            $stmt->execute();
-            if($stmt->rowCount()>0){
-                flash("All games removed successfully", "success");
-            }else{
-                flash("Games were not able to be removed from account", "warning");
-            }
-        }catch (PDOException $e){
-            flash("A database error occured, please try again later", "danger");
-            error_log(var_export($e, true), 3, "/Users/datha/Documents/IT202_Github/db624-it202-007/public_html/Project/admin/error_log.log");
-        }catch (Exception $e){
-            flash("An unknown error has occured", "danger");
-            error_log(var_export($e, true), 3, "/Users/datha/Documents/IT202_Github/db624-it202-007/public_html/Project/admin/error_log.log");
-        }
-        unset($_GET["remove_all"]);
     }
 
     $search = (isset($_GET["game_search"]))?se($_GET,"game_search","",false):NULL;
@@ -53,14 +33,14 @@ if (is_logged_in(true)) {
     $total=0;
     $offset=$page * $num_records;
     //echo $offset . " " . $num_records;
-    $results = returnSearchResults($search, $tag_search, $num_records, $order_column, $order, $api_filter, $total, $offset, "saved");
+    $results = returnSearchResults($search, $tag_search, $num_records, $order_column, $order, $api_filter, $total, $offset, "unsaved");
     //echo $total;
 
-    $query_num_saved_games = "select count(*) as `ct` from `Game_associations` where `user_id`=$user_id";
+    $query_num_saved_games = "select count(`game_id`) as `ct` from `Games_details` where `game_id` not in (select `ga`.`game_id` from `Game_associations` `ga`)";
     $num_saved_games = select($query_num_saved_games)[0]["ct"];
 ?>
 <div class="container-fluid">
-    <h3 class='mt-3 mb-3'>There are <?php echo $num_saved_games; ?> games saved in your profile</h3>
+    <h3 class='mt-3 mb-3'>There are <?php echo $num_saved_games; ?> games not saved by any user</h3>
     <hr class='mt-3 mb-3'>
     <h3 class='mt-3 mb-3'>Filter Results</h3>
     <form method="GET">
@@ -102,15 +82,10 @@ if (is_logged_in(true)) {
             <?php render_button(["text" => "Reset", "type" => "submit"]); ?>
         </form>
     </div>
-    <div class="col-3">
-        <form method="GET">
-            <?php render_input(["type"=>"hidden", "name"=>"remove_all", "value"=>"1"]); ?>
-            <?php render_button(["text" => "Remove all", "type" => "submit", "color"=>"danger"]); ?>
-        </form>
-    </div>
     
     <h4>Number of games meeting search criteria: <?php echo $total ?></h4>
     <hr class='mt-3 mb-3'>
+
     <?php if(empty($results)):?>
         <?php flash("No games were found meeting search criteria", "warning"); ?>
     <?php endif ?>
@@ -124,9 +99,9 @@ if (is_logged_in(true)) {
 </div>
 
 <?php
-require_once(__DIR__ . "/../../partials/pagination_nav.php");
+require_once(__DIR__ . "/../../../partials/pagination_nav.php");
 ?> 
 
 <?php
-require_once(__DIR__ . "/../../partials/flash.php");
+require_once(__DIR__ . "/../../../partials/flash.php");
 ?>  
